2dd6c993adea486748f335cba9059f8a17c39aac
Pinosto <juho.pinosto@hotmail.com>
2021-04-16 14:18:57 +0300

    workflow added

---
+++ app.js
@@ -0,0 +1,79 @@
+const dialogflow = require('@google-cloud/dialogflow');
+require('dotenv').config()
+const express = require('express')
+const cors = require('cors')
+const app = express()
+
+app.use(cors())
+app.use(express.json())
+app.use(express.static('build'))
+
+// Heroku dynamically sets a port
+const PORT = process.env.PORT || 5000
+
+app.use(express.static('build'))
+
+app.get('/health', (req, res) => {
+    res.send('ok')
+})
+
+app.get('/version', (req, res) => {
+  res.send('2') // change this string to ensure a new version deployed
+})
+
+app.get('/api/dialogflow', function (req, res) {
+  res.send('boilerchat')
+})
+
+app.post('/api/dialogflow', async function (req, res) {
+
+  const projectId = process.env.P_ID;
+  const sessionId = req.body.uuid;
+  const query = req.body.text;
+  const languageCode = 'en-US';
+  const credentials = {
+    client_email: process.env.C_EMAIL,
+    private_key: process.env.P_KEY.replace(/\\n/g, '\n'),
+  };
+
+  // Create a new session
+  const sessionClient = new dialogflow.SessionsClient({
+    projectId,
+    credentials,
+  });
+  const sessionPath = sessionClient.projectAgentSessionPath(projectId, sessionId);
+
+  // The text query request.
+  const request = {
+    session: sessionPath,
+    queryInput: {
+      text: {
+        text: query,
+        languageCode: languageCode,
+      },
+    },
+  };
+
+  try {
+    // Send request and log result
+    const responses = await sessionClient.detectIntent(request);
+    console.log('Detected intent');
+    const result = responses[0].queryResult;
+    console.log(`  Query: ${result.queryText}`);
+    console.log(`  Response: ${result.fulfillmentText}`);
+
+    if (result.intent) {
+      console.log(`  Intent: ${result.intent.displayName}`);
+    } else {
+      console.log(`  No intent matched.`);
+    }
+    res.json(result.fulfillmentText)
+  } catch (e) {
+    console.error(e)
+    res.json('I think dialogflow is down.')
+  }
+})
+
+app.listen(PORT, () => {
+  console.log(`Server running on port ${PORT}`)
+})
--- src/App.js
+++ src/App.js
@@ -1,19 +1,104 @@
+/* eslint-disable react-hooks/exhaustive-deps */
+import SpeechRecognition, { useSpeechRecognition } from "react-speech-recognition";
+import { useSpeechSynthesis } from 'react-speech-kit';
+import axios from 'axios'
+import { v4 as uuid } from 'uuid'
 import logo from './logo.svg';
 import './App.css';
+import { useEffect, useState } from 'react';
+const baseUrl = '/api/dialogflow'
 
-function App() {
+
+const App = () => {
+  const sessionId = uuid()
+
+  const commands = [
+    {
+      command: '*',
+      callback: (userSpeak) => setMyPhrase(userSpeak)
+    },
+  ]
+
+  const { transcript, resetTranscript } = useSpeechRecognition({ commands });
+  const { speak } = useSpeechSynthesis();
+  const [chatOn, setchatOn] = useState(false)
+  const [myPhrase, setMyPhrase] = useState('')
+  const [botPhrase, setBotPhrase] = useState('')
+  const [phraseOnScreen, setPhraseOnScreen] = useState('')
+
+  useEffect(() => {
+    if (myPhrase !== '') {
+      resetTranscript()
+      setPhraseOnScreen(myPhrase)
+      console.log(`myPhrase: ${myPhrase}`)
+      const messageObject = { text: myPhrase, uuid: sessionId }
+      axios
+        .post(baseUrl, messageObject)
+        .then(res => setBotPhrase(res.data))
+        .catch(() => setBotPhrase('Back end is taking break.'))
+      setMyPhrase('')
+    }
+  }, [myPhrase])
+
+  useEffect(() => {
+    if (botPhrase !== '') {
+      setPhraseOnScreen(botPhrase)
+      console.log(`botPhrase: ${botPhrase}`)
+      speak({ text: botPhrase })
+      setBotPhrase('')
+    }
+  }, [botPhrase])
+
+  if (!SpeechRecognition.browserSupportsSpeechRecognition()) {
+    return (
+      <div>
+        Browser is not Support Speech Recognition.
+      </div>
+    );
+  }
+
+  const handleChatOn = () => {
+    setchatOn(!chatOn)
+    setBotPhrase(`What's on Your Mind?`)
+    SpeechRecognition.startListening({
+      continuous: true,
+      language: 'en-US',
+    });
+  }
+  const handleChatOff = () => {
+    SpeechRecognition.stopListening();
+    resetTranscript();
+    setchatOn(!chatOn)
+  }
+  const notSoFast = (e) => {
+    setBotPhrase(`Hey don't leave.`)
+  }
+
+
   return (
     <div className="App">
       <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
+        {chatOn ?
+          <div>
+            <img src={logo} className="App-logo-run" alt="logo" onClick={handleChatOff} />
+            {transcript ?
+              <p>{transcript}</p>
+              :
+              <p>{phraseOnScreen}</p>
+            }
+          </div>
+          :
+          <div>
+            <img src={logo} className="App-logo-stop" alt="logo" onClick={handleChatOn} />
+            <p>Click logo to start boilerplate chat...</p>
+          </div>
+        }
         <a
           className="App-link"
-          href="https://reactjs.org"
+          href="https://github.com/Pinosto/boilerplate-chat"
           target="_blank"
           rel="noopener noreferrer"
+          onClick={notSoFast}
         >
           Learn React
         </a>
