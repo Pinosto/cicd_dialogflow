2c1880f5d93accc87ce5d5b6f0998102cffb2109
Pinosto <juho.pinosto@hotmail.com>
2021-04-16 16:04:32 +0300

    cypress testing
    #skip #notest #noslack

--- cypress/integration/dialogflow_app.js
+++ cypress/integration/dialogflow_app.js
@@ -7,7 +7,7 @@ describe('Boilerplate', function() {
     cy.visit('http://localhost:5000')
     cy.get('[alt="logo"]').click()
   })
-  it('github page link on page', function() {
+  it('github link on page', function() {
     cy.visit('http://localhost:5000')
     cy.contains('React')
   })
