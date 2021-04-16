77d42d9c54ffb14a4657171ccc2b5a87abfd8f3b
Pinosto <juho.pinosto@hotmail.com>
2021-04-16 15:56:52 +0300

    cypress fix
    #noslack

--- cypress/integration/dialogflow_app.js
+++ cypress/integration/dialogflow_app.js
@@ -3,13 +3,12 @@ describe('Boilerplate', function() {
     cy.visit('http://localhost:5000')
     cy.contains('boilerplate')
   })
-  it('github page can be opened', function() {
-    cy.visit('http://localhost:5000')
-    cy.contains('React').click()
-    cy.contains('cicd_dialogflow')
-  })
   it('can click logo', function() {
     cy.visit('http://localhost:5000')
     cy.get('[alt="logo"]').click()
+  })
+  it('github page link on page', function() {
+    cy.visit('http://localhost:5000')
+    cy.contains('React')
   })
 })
